20afaafc781d4d931c2105b98cb9f92c377ed45e
Martin Rekalde Hernando <marekald@c2r7s2.42urduliz.com>
2025-07-15 21:08:22 +0200

    ex04: reducimos a un temp

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -6,25 +6,23 @@
 /*   By: marekald <marekald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 16:18:08 by marekald          #+#    #+#             */
-/*   Updated: 2025/07/15 18:54:29 by marekald         ###   ########.fr       */
+/*   Updated: 2025/07/15 21:02:22 by marekald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int	x;
-	int	y;
+	int	temp;
 
-	x = *a;
-	y = *b;
-	*a = x / y;
-	*b = x % y;
+	temp = *a;
+	*a = temp / *b;
+	*b = temp % *b;
 }
 
 /* #include <stdio.h>
 int	main(void)
 {
-	int	a = 9;
+	int	a = 10;
 	int b = 2;
 
 	ft_ultimate_div_mod(&a, &b);
