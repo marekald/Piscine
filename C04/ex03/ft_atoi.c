4e89a62a7eafc256f8aeb1836071c5791f09152b
Martin Rekalde Hernando <marekald@c2r2s2.42urduliz.com>
2025-07-23 20:55:18 +0200

    C04 ex00-03

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marekald <marekald@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/23 19:51:17 by marekald          #+#    #+#             */
+/*   Updated: 2025/07/23 20:50:17 by marekald         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_neg(char *str, int *neg)
+{
+	int	i;
+
+	i = 0;
+	*neg = 1;
+	while (str[i] != '\0' && str[i] == ' ')
+		i++;
+	while (str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+			*neg = *neg * -1;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	neg;
+	int	num;
+
+	num = 0;
+	i = ft_neg(str, &neg);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			num = (num * 10) + (str[i] - 48);
+			i++;
+			if (!(str[i] >= '0' && str[i] <= '9'))
+				return (num * neg);
+		}
+		else
+			break ;
+	}
+	return (num * neg);
+}
+
+
+/* #include <stdio.h>
+int	main(void)
+{
+	printf("ft_atoi: %i\n", ft_atoi("  -+--54d3 21 "));
+} */
